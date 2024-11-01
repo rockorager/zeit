5041c7cac243f0d05f4578a334f52e12bb450e55
Tim Culverhouse <tim@timculverhouse.com>
2024-11-01 10:03:09 -0500

    add doctest

--- src/zeit.zig
+++ src/zeit.zig
@@ -1528,3 +1528,67 @@ test "gofmt" {
     try time3.gofmt(writer, "frac .999999999");
     try std.testing.expectEqualStrings("frac .123456", fbs.getWritten());
 }
+
+test Instant {
+    const zeit = @This();
+
+    const alloc = std.testing.allocator;
+    var env = try std.process.getEnvMap(alloc);
+    defer env.deinit();
+
+    // Get an instant in time. The default gets "now" in UTC
+    const now = try instant(.{});
+
+    // Load our local timezone. This needs an allocator. Optionally pass in a
+    // *const std.process.EnvMap to support TZ and TZDIR environment variables
+    const local_tz = try zeit.local(alloc, &env);
+    defer local_tz.deinit();
+
+    // Convert our instant to a new timezone
+    const now_local = now.in(&local_tz);
+
+    // Generate date/time info for this instant
+    const dt = now_local.time();
+
+    // Print it out
+    std.log.info("{}", .{dt});
+
+    // zeit.Time{
+    //    .year = 2024,
+    //    .month = zeit.Month.mar,
+    //    .day = 16,
+    //    .hour = 8,
+    //    .minute = 38,
+    //    .second = 29,
+    //    .millisecond = 496,
+    //    .microsecond = 706,
+    //    .nanosecond = 64
+    //    .offset = -18000,
+    // }
+
+    const anywriter = std.io.null_writer.any();
+    // Format using strftime specifier. Format strings are not required to be comptime
+    try dt.strftime(anywriter, "%Y-%m-%d %H:%M:%S %Z");
+
+    // Or...golang magic date specifiers. Format strings are not required to be comptime
+    try dt.gofmt(anywriter, "2006-01-02 15:04:05 MST");
+
+    // Load an arbitrary location using IANA location syntax. The location name
+    // comes from an enum which will automatically map IANA location names to
+    // Windows names, as needed. Pass an optional EnvMap to support TZDIR
+    const vienna = try zeit.loadTimeZone(alloc, .@"Europe/Vienna", &env);
+    defer vienna.deinit();
+
+    // Parse an Instant from an ISO8601 or RFC3339 string
+    _ = try zeit.instant(.{
+        .source = .{
+            .iso8601 = "2024-03-16T08:38:29.496-1200",
+        },
+    });
+
+    _ = try zeit.instant(.{
+        .source = .{
+            .rfc3339 = "2024-03-16T08:38:29.496706064-1200",
+        },
+    });
+}

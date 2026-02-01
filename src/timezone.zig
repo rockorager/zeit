365cdfade00f6fad5a04197d828720faaf9b5b36
Krzysztof Wolicki <der.teufel.mail@gmail.com>
2026-02-01 16:26:09 +0100

    Fix compile errors in timezone

--- src/timezone.zig
+++ src/timezone.zig
@@ -117,7 +117,7 @@ pub const Posix = struct {
                     const julian = try std.fmt.parseInt(u9, str[1..], 10);
                     return .{ .julian = .{ .day = julian } };
                 },
-                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' => |_| {
+                '0', '1', '2', '3', '4', '5', '6', '7', '8', '9' => {
                     const julian = try std.fmt.parseInt(u9, str, 10);
                     return .{ .julian_leap = .{ .day = julian } };
                 },
@@ -795,7 +795,7 @@ pub const Windows = struct {
     /// 4. Determine if we are in DST or not
     /// 5. Return result
     pub fn adjust(self: Windows, timestamp: Seconds) AdjustedTime {
-        const instant = zeit.instant(.{ .source = .{ .unix_timestamp = timestamp } }) catch unreachable;
+        const instant = zeit.instant(undefined, .{ .source = .{ .unix_timestamp = timestamp } }) catch unreachable;
         const time = instant.time();
 
         const systemtime: windows.SYSTEMTIME = .{
